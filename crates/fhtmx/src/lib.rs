d746436c499891a2c5589780e623d9da8117f499
renato <rhermoza145@gmail.com>
2026-01-13 18:07:02 -0500

    feat: add js

--- crates/fhtmx/src/lib.rs
+++ crates/fhtmx/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod attribute;
 pub mod element;
 pub mod html_page;
+pub mod js;
 pub mod node;
 pub mod render;
 pub mod utils;
@@ -9,6 +10,7 @@ pub mod prelude {
     pub use crate::attribute::*;
     pub use crate::element::*;
     pub use crate::html_page::*;
+    pub use crate::js::*;
     pub use crate::node;
     pub use crate::render::*;
     pub use crate::utils::*;
