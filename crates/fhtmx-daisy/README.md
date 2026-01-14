da2812c30c618cf8b29bd2669701d8c8752ba4e4
renato <rhermoza145@gmail.com>
2026-01-14 15:03:18 -0500

    feat: start daisy crate

---
+++ crates/fhtmx-daisy/README.md
@@ -0,0 +1,5 @@
+# fhtmx-daisy
+
+Create html using `fhtmx` using [daisyUI](https://daisyui.com/).
+
+TODO: fill documentation
---
+++ crates/fhtmx-daisy/src/lib.rs
@@ -0,0 +1,2 @@
+// const DAISY_LINK: &str = "https://cdn.jsdelivr.net/npm/daisyui@5";
+// const TW_SCR: &str = "https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4";
