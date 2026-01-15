5fd9ee32d9360132ed9a34aecf7fcd98a7b944e3
renato <rhermoza145@gmail.com>
2026-01-15 14:26:36 -0500

    feat: set prelude for daisy

--- crates/fhtmx-daisy/src/lib.rs
+++ crates/fhtmx-daisy/src/lib.rs
@@ -3,7 +3,18 @@ pub mod avatar;
 pub mod button;
 pub mod dropdown;
 pub mod fab;
-pub mod macros;
+mod macros;
 pub mod modal;
 pub mod swap;
 pub mod utils;
+
+pub mod prelude {
+    pub use crate::accordion::*;
+    pub use crate::avatar::*;
+    pub use crate::button::*;
+    pub use crate::dropdown::*;
+    pub use crate::fab::*;
+    pub use crate::modal::*;
+    pub use crate::swap::*;
+    pub use crate::utils::*;
+}
