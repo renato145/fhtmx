cac21546b19161ef48e75b92b0ae9172d91f0884
renato <rhermoza145@gmail.com>
2026-01-23 16:31:21 -0500

    chore: add derive crate to release config

--- crates/fhtmx/tests/html_view.rs
+++ crates/fhtmx/tests/html_view.rs
@@ -13,6 +13,25 @@ fn macro_works() {
         age: 20,
     };
     let res = x.html_view().render();
-    println!("{res}");
-    // insta::assert_snapshot!(res, @"");
+    insta::assert_snapshot!(res, @r#"
+    <div class="card">
+      <div class="card-body">
+        <h2 class="card-title">User info</h2>
+        <ul class="list">
+          <li class="list-row p-1">
+            <div class="font-bold">name</div>
+            <div>
+              <p>Karls</p>
+            </div>
+          </li>
+          <li class="list-row p-1">
+            <div class="font-bold">age</div>
+            <div>
+              <p>20</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    "#);
 }
